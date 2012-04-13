de548039654b23ed358103fead44ed35117bfe7c
David <dzd1@humboldt.edu>
2012-04-12 20:51:20 -0700

    Cleanin it up a bit

--- test.py
+++ test.py
@@ -6,19 +6,25 @@ import os
 
 
 
-
+#Set a mixer object to mixer, grab just the Master channel
 mixer = alsaaudio.Mixer('Master', 0)
+
+#Grab the volume of the channel
 volumes = mixer.getvolume()
 
+#did it grab stuff?
 print mixer.mixer()
 print volumes
 
+
+#change the volumes to a string so we can read it with the text to speech tool
 volumes = ''.join(map(str,volumes))
 
 
 
 def tts(text):
-      return os.system("espeak  -s 155 -a 200 "+text+" " )
+	'''built in text to speech for linux'''
+	return os.system("espeak  -s 155 -a 200 "+text+" " )
 
 
 
@@ -28,14 +34,6 @@ m = datetime.datetime.now().strftime("%I %M %S")
 if volumes == '100': 
 	tts("'Bro your volume is maxed out'")
 
-'''if volumes != '100':
+if volumes != '100':
 	volumes = volumes + '%'
 	tts("'Bro your volume is set to'" + volumes)
-'''
-
-def lsser():
-	return os.system("ls")
-
-
-
-lsser()
